3cd303255f493fc2297890f62fdd896eab936ea5
Hao Lian <hi@haolian.org>
2016-08-29 13:39:37 -0400

    a textarea for these modern times

--- web/components/wall.js
+++ web/components/wall.js
@@ -16,7 +16,6 @@ export default class Wall extends Component {
   componentDidMount() {
     _fetchJSON("http://localhost:3000/api/")
       .then(data => {
-        console.log("did mount", data)
         this.setState({
           loading: false,
           avatarURL: _a(data.avatarURL),
@@ -28,19 +27,33 @@ export default class Wall extends Component {
 
   render() {
     const state = _a(this.state);
-    if (state.loading) {
-      return (
-        <p id="wall-loading">reticulating splines...</p>
-      )
-    } else {
-      return (
-        <div id="wall-done">
-          <p className="name">{state.name}</p>
+    if (state.loading) return this._renderLoading();
+    else return this._renderLoaded();
+  }
+
+  _renderLoading() {
+    return (
+      <p id="wall-loading">reticulating splines...</p>
+    )
+  }
+
+  _renderLoaded() {
+    const state = _a(this.state);
+    return (
+      <div id="wall-done">
+        <h1 className="name">{state.name}</h1>
+        <p>
+          <img src={state.avatarURL} />
+        </p>
+        <form method="post" action="http://localhost:3000/api/posts">
           <p>
-            <img src={state.avatarURL} />
+            <textarea placeholder={"post something to " + state.name + "'s wall"} name="textarea" rows="7" cols="40" className="css-textarea"></textarea>
           </p>
-        </div>
-      )
-    }
+          <p>
+            <input type="submit" value="post to wall"></input>
+          </p>
+        </form>
+      </div>
+    )
   }
 }
